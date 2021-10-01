3e100b5563626c12c840548f998c0e7a285be337
bofnb <ihasbacon247@gmail.com>
2021-09-30 19:05:38 -0500

    Added comments on dolapes.cpp and created program that allows to calculate the average of three grades

---
+++ cali3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int cali1;
+int cali2;
+int cali3;
+int promedio;
+
+int main(){
+//Lets users write each grade, then averages them out.
+cout<<"Escribe la primera calificacion\n";
+cin>>cali1;
+cout<<"Escribe la segunda calificacion\n";
+cin>>cali2;
+cout<<"Escribe la tercera calificacion\n";
+cin>>cali3;
+promedio=(cali1+cali2+cali3)/3;
+cout<<"El promedio es "<< promedio;
+
+return 0;
+
+
+
+
+
+
+
+
+
+
+}
--- dolapes.cpp
+++ dolapes.cpp
@@ -5,11 +5,13 @@ int dolares;
 int pesos;
 
 int main(){
+	//Allows users to input an amount in dollars and then does the convertion
+	//on line 15
 cout<<"Conversion de Dolares a Pesos\n";
 cout<<"Escriba cantidad en dolares a cambiar a pesos\n";
-
+	
 cin>> dolares;
-
+	
 pesos= dolares * 20.57;
 
 cout<<"La conversion de dolares a pesos es\n" << pesos <<"\n";
